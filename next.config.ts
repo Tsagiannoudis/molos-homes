3074bf96da2e584b5f47773c0d296c6501d35b95
Tsagiannoudis <i.tsagiannoudis@gmail.com>
2025-11-09 16:38:02 +0200

    fixed domain valid

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  async rewrites() {
+    return [
+      // Αυτή η ανακατεύθυνση είναι πολύ γενική και θα ανακατευθύνει όλα τα αιτήματα στην αρχική σελίδα.
+      // Ενδέχεται να μην είναι αυτό που θέλετε, καθώς θα απενεργοποιήσει όλες τις άλλες σελίδες και API routes.
+      // Αν χρειάζεστε ανακατευθύνσεις, ορίστε τις πιο συγκεκριμένα.
+      {
+        source: '/:path*',
+        destination: '/',
+      },
+      //Η παρακάτω γραμμή είναι συνήθως περιττή, καθώς το Next.js διαχειρίζεται το /_next/ αυτόματα.
+      {
+        source: '/_next/:path*',
+        destination: '/_next/:path*',
+      },
+    ]
+  }
+}
 
 export default nextConfig;
