7fdf08827430ef6d03201ad2c7692f6617f57e82
Tsagiannoudis <i.tsagiannoudis@gmail.com>
2026-01-13 20:08:10 +0200

    update gallary for video

--- components/constructions/ProjectGallery.tsx
+++ components/constructions/ProjectGallery.tsx
@@ -3,7 +3,9 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import Lightbox from 'yet-another-react-lightbox';
+import Video from 'yet-another-react-lightbox/plugins/video';
 import 'yet-another-react-lightbox/styles.css';
+import { PlayCircleIcon } from '@heroicons/react/24/solid';
 
 type ProjectGalleryProps = {
   photos: string[];
@@ -17,7 +19,20 @@ const ProjectGallery = ({ photos, projectTitle }: ProjectGalleryProps) => {
     return null; // Μην εμφανίζεις τίποτα αν δεν υπάρχουν φωτογραφίες
   }
 
-  const slides = photos.map(photo => ({ src: photo }));
+  const slides = photos.map(photo => {
+    if (photo.endsWith('.mp4')) {
+      return {
+        type: 'video' as const,
+        sources: [
+          {
+            src: photo,
+            type: 'video/mp4',
+          },
+        ],
+      };
+    }
+    return { src: photo };
+  });
 
   return (
     <div>
@@ -29,12 +44,21 @@ const ProjectGallery = ({ photos, projectTitle }: ProjectGalleryProps) => {
             className="relative w-full aspect-square rounded-lg overflow-hidden shadow-md transform hover:scale-105 transition-transform duration-300 cursor-pointer"
             onClick={() => setIndex(i)}
           >
-            <Image src={photo} alt={`Gallery image ${i + 1} for ${projectTitle}`} fill className="object-cover" sizes="(max-width: 640px) 50vw, 33vw" />
+            {photo.endsWith('.mp4') ? (
+              <>
+                <video src={photo} className="object-cover w-full h-full" muted playsInline loop />
+                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30">
+                  <PlayCircleIcon className="w-16 h-16 text-white" />
+                </div>
+              </>
+            ) : (
+              <Image src={photo} alt={`Gallery image ${i + 1} for ${projectTitle}`} fill className="object-cover" sizes="(max-width: 640px) 50vw, 33vw" />
+            )}
           </div>
         ))}
       </div>
 
-      <Lightbox open={index >= 0} close={() => setIndex(-1)} slides={slides} index={index} />
+      <Lightbox open={index >= 0} close={() => setIndex(-1)} slides={slides} index={index} plugins={[Video]} />
     </div>
   );
 };
