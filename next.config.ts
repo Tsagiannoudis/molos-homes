77a69be39b23ce07510e3dcc011432fe3b3d001a
Tsagiannoudis <i.tsagiannoudis@gmail.com>
2026-01-13 20:14:00 +0200

    nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      // ✅ Πρώτα επιτρέπουμε τα static αρχεία να περάσουν κανονικά
-      {
-        source: '/_next/:path*',
-        destination: '/_next/:path*',
-      },
-      // ✅ Μετά, αν πραγματικά θες όλα τα υπόλοιπα να δείχνουν στην αρχική
-      // (π.χ. single-page app συμπεριφορά)
-      {
-        source: '/:path*',
-        destination: '/',
-      },
-    ];
-  },
+ 
 };
 
 export default nextConfig;
